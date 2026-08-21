0a2d27356ae07efe3708ac76600e94a9b4482244
Blessan <blassan8@gmail.com>
2026-08-21 10:10:28 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+
+export default tseslint.config(
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [js.configs.recommended, tseslint.configs.recommended, reactRefresh.configs.vite],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs["recommended-latest"].rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: [
+            "useTheme",
+            "useScan",
+            "SEVERITY_DOT",
+            "SEVERITY_HEX",
+            "DEFAULT_FILTER_STATE",
+            "applyIssueFilters",
+          ],
+        },
+      ],
+    },
+  }
+);
